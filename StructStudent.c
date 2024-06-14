79de14f23abecb8664d9e4c2684c71c9a874c99c
sambaliga06 <118614578+sambaliga06@users.noreply.github.com>
2024-06-14 12:06:36 +0530

    Update StructStudent.c

--- StructStudent.c
+++ StructStudent.c
@@ -1,3 +1,4 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 
 struct student{
@@ -10,10 +11,10 @@ struct student{
 int main() {
     // Write C code here
 
-    struct student detail[5];
+    struct student detail[3];
     int i,j;
-    float total[5];
-    for(i=0;i<5;i++){
+    float total[3];
+    for(i=0;i<3;i++){
     scanf("%d",&detail[i].roll_no);
     scanf("%s",detail[i].name);
     total[i]=0;
@@ -25,13 +26,23 @@ int main() {
     
     
     
-    for(i=0;i<5;i++){
-    printf("name %s\n",detail[i].name);
+    for(i=0;i<3;i++){
+    printf("*****\nName %s\n",detail[i].name);
     printf("roll NO %d\n",detail[i].roll_no);
     for(j=0;j<3;j++){
     printf("mark in subject %d = %f\n",j+1,detail[i].mark[j]);
 }
         printf("total mark is %f \n",total[i]);
     }
+    
+    int k=0,max=total[0];
+    for(i=1;i<3;i++){
+   if(max<total[i]){
+       max=total[i];
+       k=i;
+   }
+        
+    }
+    printf("Topper is %s  with %f marks",detail[k].name,total[k]);
     return 0;
 }
